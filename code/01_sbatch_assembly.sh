16057d30a15eb1de1bffa6d9737a326bfe203d5f
Lovisa Lundberg <lovilund@IBG-D0020.ibg.uu.se>
2018-04-19 15:23:25 +0200

    Updating the sbatch script with directories

--- code/01_sbatch_assembly.sh
+++ code/01_sbatch_assembly.sh
@@ -1,18 +1,21 @@
 #!/bin/bash -l
 
-#SBATCH -A g2018003
+#SBATCH -A g2018003_6
 #SBATCH -p core
 #SBATCH -n 2
 #SBATCH -t 13:00:00
-#SBATCH -J 01_assembly
+#SBATCH -J 01_DNA_assembly
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user lovisa.lundberg.4321@student.uu.se
 
 #Load modules
-module load bioinfo-tools
+module load bioinfo-tools canu/1.7
 
 #Commands
 canu -assemble \
--p 01_genome_assembly  -d #directory  \
-genomeSize= \
-stopOnReadQuality=false \  
+-p leptospirillum \  
+-d /home/lolu4321/genome_analysis_LovisaLundberg/analyses  \
+genomeSize=3m \
+stopOnReadQuality=false \
+-pacbio-raw 
+/home/lolu4321/genome_analysis_LovisaLundberg/data/DNA_raw_data/ERR2028*.fastq.gz  
